a8d744a99ffdd3411e4121c6b32da1d876131173
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-12-08 21:48:32 +0100

    Day 4 part 2

--- lib/aoc2016/day4.ex
+++ lib/aoc2016/day4.ex
@@ -25,23 +25,18 @@ defmodule Aoc2016.Day4 do
   1514
   """
   def real_rooms_ids_sum(rooms_str) do
+    valid_rooms(rooms_str)
+    |> Enum.reduce(0, fn(x, acc) -> String.to_integer(x["id"]) + acc end)
+  end
+
+  defp valid_rooms(rooms_str) do
     rooms_str
     |> String.split("\n")
-    |> Enum.map(fn(room_str) -> String.strip(room_str) |> id end)
-    |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+    |> Enum.map(fn(room_str) -> room_str |> String.strip |> parse end)
+    |> Enum.filter(&valid?/1)
   end
 
-  defp id(%{"name" => name, "hash" => hash, "id" => id}) do
-    id(valid?(name, hash), id)
-  end
-  defp id(room_str), do: room_str |> parse |> id
-  defp id(true, id) do
-    {i, _ } = Integer.parse(id)
-    i
-  end
-  defp id(_, _), do: 0
-
-  defp valid?(name, hash) do
+  defp valid?(%{"name" => name, "hash" => hash}) do
     name
     |> String.replace("-", "")
     |> String.graphemes
@@ -54,8 +49,54 @@ defmodule Aoc2016.Day4 do
     |> Enum.join("") == hash
   end
 
-  defp parse(room) do
+  defp parse(room_str) do
     Regex.named_captures(
-      ~r/(?<name>([a-zA-Z]+-)+)(?<id>\d{3})\[(?<hash>[a-zA-Z]{5})]/, room)
+      ~r/(?<name>([a-zA-Z]+-)+)(?<id>\d{3})\[(?<hash>[a-zA-Z]{5})]/, room_str)
   end
+
+  @doc ~S"""
+  With all the decoy data out of the way, it's time to decrypt this list and get
+  moving.
+
+  The room names are encrypted by a state-of-the-art shift cipher, which is
+  nearly unbreakable without the right software. However, the information kiosk
+  designers at Easter Bunny HQ were not expecting to deal with a master
+  cryptographer like yourself.
+
+  To decrypt a room name, rotate each letter forward through the alphabet a
+  number of times equal to the room's sector ID. A becomes B, B becomes C, Z
+  becomes A, and so on. Dashes become spaces.
+
+  For example, the real name for "qzmt-zixmtkozy-ivhz-343" is "very encrypted
+  name".
+
+  iex> Aoc2016.Day4.decode("qzmt-zixmtkozy-ivhz-343[asdfg]")
+  "very encrypted name"
+  """
+  def decode(room_str), do: room_str |> parse |> do_decode
+
+  defp do_decode(%{"name" => name, "id" => id}) do
+    nr = String.to_integer(id)
+    name
+    |> String.replace("-", " ")
+    |> String.trim
+    |> String.to_charlist
+    |> Enum.map(&do_decode(&1, nr))
+    |> to_string
+  end
+  defp do_decode(32, _), do: 32
+  defp do_decode(ch, nr), do: rem(ch - 97 + nr, 26) + 97
+
+
+  def find_room_sector_id(rooms_str) do
+    rooms_str
+    |> valid_rooms
+    |> Enum.find(fn(x) ->
+        Regex.match?(~r/.*north.*/, do_decode(x))
+    end)
+    |> id
+  end
+
+  defp id(nil), do: :error
+  defp id(%{"id" => id}), do: String.to_integer(id)
 end
