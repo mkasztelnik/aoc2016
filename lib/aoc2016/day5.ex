2542dde4a423087511f7090371e23810a837c8da
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-12-09 09:26:30 +0100

    Day 5 part 2

--- lib/aoc2016/day5.ex
+++ lib/aoc2016/day5.ex
@@ -27,24 +27,93 @@ defmodule Aoc2016.Day5 do
   In this example, after continuing this search a total of eight times, the
   password is 18f47a30.
 
-  #iex> Aoc2016.Day5.password("abc", 8)
+  #iex> Aoc2016.Day5.password("abc")
   "18f47a30"
   """
-  def password(input, count) do
-    gen_pass(input, 0, "", count)
+  def password(input) do
+    gen_pass(input, 0, "", 8)
   end
 
   def gen_pass(_, _, pass, 0), do: pass
   def gen_pass(input, nr, pass , count) do
-    {ch, nr} = gen_next_pass_char(input, nr)
-    gen_pass(input, nr + 1, pass <> ch, count - 1)
+    {rest, nr} = gen_next_pass_char(input, nr)
+    gen_pass(input, nr + 1, pass <> String.first(rest), count - 1)
   end
 
   defp gen_next_pass_char(input, nr) do
     gen_next_pass_char(md5(input <> to_string(nr)), input, nr)
   end
-  defp gen_next_pass_char("00000" <> rest, _, nr), do: {String.first(rest), nr}
+  defp gen_next_pass_char("00000" <> rest, _, nr), do: {rest, nr}
   defp gen_next_pass_char(_, input, nr), do: gen_next_pass_char(input, nr + 1)
 
   defp md5(str), do: Base.encode16(:erlang.md5(str), case: :lower)
+
+  @doc ~S"""
+  As the door slides open, you are presented with a second door that uses a
+  slightly more inspired security mechanism. Clearly unimpressed by the last
+  version (in what movie is the password decrypted in order?!), the Easter Bunny
+  engineers have worked out a better solution.
+
+  Instead of simply filling in the password from left to right, the hash now
+  also indicates the position within the password to fill. You still look for
+  hashes that begin with five zeroes; however, now, the sixth character
+  represents the position (0-7), and the seventh character is the character to
+  put in that position.
+
+  A hash result of 000001f means that f is the second character in the password.
+  Use only the first result for each position, and ignore invalid positions.
+
+  For example, if the Door ID is abc:
+
+    - The first interesting hash is from abc3231929, which produces 0000015...;
+      so, 5 goes in position 1: _5______.
+
+    - In the previous method, 5017308 produced an interesting hash; however,
+      it is ignored, because it specifies an invalid position (8).
+
+    - The second interesting hash is at index 5357525, which produces 000004e...;
+      so, e goes in position 4: _5__e___.
+
+  You almost choke on your popcorn as the final character falls into place,
+  producing the password 05ace8e3.
+
+  # iex> Aoc2016.Day5.password2("abc")
+  # "05ace8e3"
+  """
+  def password2(input) do
+    pass = {nil, nil, nil, nil, nil, nil, nil, nil}
+    gen_pass2(input, 0, pass)
+    |> Tuple.to_list
+    |> Enum.join("")
+  end
+
+  defp gen_pass2(input, nr, pass) do
+    case valid?(pass) do
+      true -> pass
+      _ ->
+          {ch, next_nr} = gen_next_pass_char(input, nr + 1)
+          gen_pass2(input, next_nr, pass, ch)
+    end
+  end
+  defp gen_pass2(input, nr, pass, hash_rest) do
+    case String.first(hash_rest) |> Integer.parse do
+      {8, _}  -> gen_pass2(input, nr + 1, pass)
+      {9, _}  -> gen_pass2(input, nr + 1, pass)
+      {x, _} -> gen_pass2(input, nr + 1, update_pass(pass, x, String.at(hash_rest, 1)))
+      :error  -> gen_pass2(input, nr + 1, pass)
+    end
+  end
+
+  defp update_pass(pass, possition, ch) do
+    case elem(pass, possition) do
+      nil -> pass |> Tuple.delete_at(possition) |> Tuple.insert_at(possition, ch)
+      _ -> pass
+    end
+  end
+
+  defp valid?(pass) do
+    pass
+    |> Tuple.to_list
+    |> Enum.find_index(fn(x) -> x == nil end) == nil
+  end
 end
