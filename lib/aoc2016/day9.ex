a73b043fd9e9c44ad4ec60d0a014e4c2526e27fe
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-12-15 22:27:39 +0100

    Day 9 part 2

--- lib/aoc2016/day9.ex
+++ lib/aoc2016/day9.ex
@@ -62,11 +62,7 @@ defmodule Aoc2016.Day9 do
   end
 
   defp do_extend(["(" | t], result) do
-    rest = Enum.join(t, "")
-    %{"size" => size, "n" => n, "rest" => rest} =
-      Regex.named_captures(~r/(?<size>\d+)x(?<n>\d+)\)(?<rest>.*)/, rest)
-    %{"subsequent" => subsequent, "rest" => rest} =
-      Regex.named_captures(~r/(?<subsequent>.{0,#{size}})(?<rest>.*)/, rest)
+    %{"n" => n, "subsequent" => subsequent, "rest" => rest} = parse(t)
 
     rest
     |> String.graphemes
@@ -74,4 +70,71 @@ defmodule Aoc2016.Day9 do
   end
   defp do_extend([], result), do: result |> Enum.reverse() |> Enum.join("")
   defp do_extend([char | rest], result), do: do_extend(rest, [char | result])
+
+  defp parse(tab) do
+    rest = Enum.join(tab, "")
+    %{"size" => size, "n" => n, "rest" => rest} =
+      Regex.named_captures(~r/(?<size>\d+)x(?<n>\d+)\)(?<rest>.*)/, rest)
+    %{"subsequent" => subsequent, "rest" => rest} =
+      Regex.named_captures(~r/(?<subsequent>.{0,#{size}})(?<rest>.*)/, rest)
+
+    %{"n" => n, "subsequent" => subsequent, "rest" => rest}
+  end
+
+  @doc ~S"""
+  Apparently, the file actually uses version two of the format.
+
+  In version two, the only difference is that markers within decompressed data
+  are decompressed. This, the documentation explains, provides much more
+  substantial compression capabilities, allowing many-gigabyte files to be
+  stored in only a few kilobytes.
+
+  For example:
+
+    - (3x3)XYZ still becomes XYZXYZXYZ, as the decompressed section contains no
+      markers.
+
+    - X(8x2)(3x3)ABCY becomes XABCABCABCABCABCABCY, because the decompressed data
+      from the (8x2) marker is then further decompressed, thus triggering the (3x3)
+      marker twice for a total of six ABC sequences.
+
+    - (27x12)(20x12)(13x14)(7x10)(1x12)A decompresses into a string of A repeated
+      241920 times.
+
+    - (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN becomes 445
+      characters long.
+
+  Unfortunately, the computer you brought probably doesn't have enough memory to
+  actually decompress the file; you'll have to come up with another way to get
+  its decompressed length.
+
+  What is the decompressed length of the file using this improved format?
+
+  iex> Aoc2016.Day9.extended_length("(3x3)XYZ")
+  9
+
+  iex> Aoc2016.Day9.extended_length("X(8x2)(3x3)ABCY")
+  20
+
+  iex> Aoc2016.Day9.extended_length("(27x12)(20x12)(13x14)(7x10)(1x12)A")
+  241920
+
+  iex> Aoc2016.Day9.extended_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")
+  445
+  """
+  def extended_length(code) do
+    code
+    |> String.graphemes
+    |> calculate_length(0)
+  end
+
+  defp calculate_length(["(" | t], code_size) do
+    %{"n" => n, "subsequent" => subsequent, "rest" => rest} = parse(t)
+
+    rest
+    |> String.graphemes
+    |> calculate_length(code_size + String.to_integer(n) * extended_length(subsequent))
+  end
+  defp calculate_length([], code_size), do: code_size
+  defp calculate_length([_ | rest], code_size), do: calculate_length(rest, code_size + 1)
 end
